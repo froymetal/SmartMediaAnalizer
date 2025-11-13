fbbad201df89809fadea10a0504288bb2f8757ec
Froy <froylan10@hotmail.com>
2025-11-13 16:28:28 -0600

    Initial commit

---
+++ SmartMediaAnalizer/SmartMediaAnalizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMediaAnalizerApp.swift
+//  SmartMediaAnalizer
+//
+//  Created by Froylan Almeida on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartMediaAnalizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartMediaAnalizer/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartMediaAnalizer
+//
+//  Created by Froylan Almeida on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
